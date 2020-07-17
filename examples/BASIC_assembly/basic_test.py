b7f28fc9d7fc30136d2965f49e4c1dd27b1ff514
Olivia Gallupová <olivia.gallup@gmail.com>
2020-07-17 17:04:06 +0200

    organization of repo

---
+++ examples/BASIC_assembly/basic_test.py
@@ -0,0 +1,11 @@
+# /**
+#  * @author Olivia Gallupová
+#  * @email olivia.gallupova@gmail.com
+#  * @create date 2020-07-17 15:11:47
+#  * @modify date 2020-07-17 15:11:47
+#  * @desc [description]
+#  */
+
+from ...input.construct import Construct
+
+design = Construct
---
+++ input/construct.py
@@ -0,0 +1,17 @@
+# /**
+#  * @author Olivia Gallupová
+#  * @email olivia.gallupova@gmail.com
+#  * @create date 2020-07-17 11:18:14
+#  * @modify date 2020-07-17 11:18:14
+#  * @desc [description]
+#  */
+
+
+class Construct():
+    def __init__(self, sbol_input):
+
+        self.id = ""
+        self.parts = ""
+
+        pass
+
