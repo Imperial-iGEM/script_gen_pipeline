cf820e946be6e93ca2c0dcd6e561a7434d6c9eee
Gabrielle Johnston <41130455+GabrielleJohnston@users.noreply.github.com>
2020-07-22 12:16:18 +0100

    Updates to basic.py

--- protocol/basic.py
+++ protocol/basic.py
@@ -1,6 +1,7 @@
 """BASIC assembly design process and steps."""
 
-from typing import List
+from collections import defaultdict
+from typing import Dict, List, Set, Tuple, Iterable, Optional
 import pandas as pd
 
 from Bio.Restriction.Restriction import RestrictionType
@@ -17,10 +18,12 @@ from Bio.SeqRecord import SeqRecord
 
 from script_gen_pipeline.protocol.instructions import Instruction, instr_to_txt, Temperature
 from script_gen_pipeline.labware.containers_copy import Container, Fridge, Well
-from script_gen_pipeline.designs.construct import Construct
+from script_gen_pipeline.designs.construct import Construct, Module, Part
 from script_gen_pipeline.protocol.protocol import Protocol, Step
 
 #BASIC_MIX = Mix()
+
+SOURCE_DECK_POS = ['2', '5', '8', '7', '10', '11'] # example pos, used in DNABot
 
 class Basic(Protocol):
     """ BASIC assembly
@@ -37,12 +40,17 @@ class Basic(Protocol):
     def __init__(self, 
         construct: Construct = Construct(),
         name: str = "",
+        source_wells: Dict[str, str] = []
     ):
         super().__init__(name=name, construct=construct)
         #self.mix = mix
+        self.source_wells = source_wells
+        # source well keys in form ['well', 'deck/plate']
+        # source well values in form [Part, Part, ...] or [Part]
 
     def run(self):
-        clip_df = self._create_clip_df()
+        self.clip_df = self._create_clip_df()
+        self.source_df = self._check_source_wells()
 
     def _create_clip_df(self):
         clips_info = {'prefixes': [], 'parts': [],
@@ -58,7 +66,19 @@ class Basic(Protocol):
                 else:
                     suffix_linker = self.construct.modules[index + 1].parts[0]
                     clips_info['suffixes'].append(suffix_linker)
-        return pd.DataFrame.from_dict(clips_info)
+        clips_df = pd.DataFrame.from_dict(clips_info)
+        return clips_df
+        
+    def _check_source_wells(self):
+        if not source_wells:
+            # generate well positions for parts, include warning
+        else:
+            # check to ensure all parts included in dict values
+            # gen well positions for parts if any missing wells, include warning
+
+        # return dataframe for easier lookup
+
+
 
             
 
